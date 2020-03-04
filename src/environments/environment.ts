b59cad29b52fa876603aae9fa7f466984a7307e0
Rick <riicardoserrano@gmail.com>
2020-03-04 17:29:35 -0400

    Hosted firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCbCPn8IDNl88s3-lijwUO1VovvXUYthf4",
+    authDomain: "microproyecto-dos-rs.firebaseapp.com",
+    databaseURL: "https://microproyecto-dos-rs.firebaseio.com",
+    projectId: "microproyecto-dos-rs",
+    storageBucket: "microproyecto-dos-rs.appspot.com",
+    messagingSenderId: "666307045502",
+    appId: "1:666307045502:web:fd95e93afac1e40631502f",
+    measurementId: "G-6W3WX7MJVB"
+  }
 };
 
 /*
