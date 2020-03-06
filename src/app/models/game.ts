6380a08ccee2eb5bfbd146e78fd1d9602b949784
Rick <riicardoserrano@gmail.com>
2020-03-06 13:42:52 -0400

    starting home

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -1,5 +1,11 @@
-class GameModel{
-    game = {
-        
-    }
+export class Game {
+    titulo: string
+    descripcion: string
+    fecha: Date
+    genero: string
+    desPublic: string
+    rating: string
+    pataforma: string
+    screenshot: string[]
+    video: string
 }
---
+++ src/app/services/firestore/firestore.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FirestoreService {
+  constructor(
+    private firestore: AngularFirestore
+  ) { }
+  //Create
+  public create(data: any, collection: string) {
+    return this.firestore.collection(collection).add(data);
+  }
+  //Read
+  public get(documentId: string, collection: string) {
+    return this.firestore.collection(collection).doc(documentId).snapshotChanges();
+  }
+  //Get
+  public getAll(collection: string) {
+    return this.firestore.collection(collection).snapshotChanges();
+  }
+  //Update
+  public update(documentId: string, data: any, collection: string) {
+    return this.firestore.collection(collection).doc(documentId).set({data}, { merge: true});
+  }
+}
